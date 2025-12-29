a130977c652e67c4bb24d064127eb8c751e7f60a
Aadya Pathak <pathakaadya19@gmail.com>
2025-12-29 17:09:02 +0530

    fixed an error

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 console.log("Main.js is loaded and running.");
-console.alert("Hello from main.js!");
+alert("Hello from main.js!");
 console.warn("This is a warning");
 alert("This is an alert from main.js");
+//let,const, var(not used anymore since its global scope)
+console.log(age);
+//strings,numbers,boolean,null,undefined,symbol
+const name="Aditya";
+const age= 30;
+const isCool= true;
+const rating=4.5;
+const x = null;
+const y = undefined;
+let z;
+console.log(typeof z);
