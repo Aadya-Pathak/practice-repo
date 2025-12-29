5eda6e3b034075b0bb152996fe42378552314537
Aadya Pathak <pathakaadya19@gmail.com>
2025-12-29 17:25:46 +0530

    performed operations on string

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ alert("Hello from main.js!");
 console.warn("This is a warning");
 alert("This is an alert from main.js");
 //let,const, var(not used anymore since its global scope)
-console.log(age);
+const a=30;
+console.log(a);
 //strings,numbers,boolean,null,undefined,symbol
 const name="Aditya";
 const age= 30;
@@ -13,3 +14,15 @@ const x = null;
 const y = undefined;
 let z;
 console.log(typeof z);
+//concatenation
+console.log("My name is " + name + " and I am " + age); 
+//template string
+console.log(`My name is ${name} and I am ${age}`);
+//string methods
+const s="Hello World";
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.substring(0,5).toLowerCase());
+console.log(s.split(''));
+const h="technology, computers, it, code";
+console.log(h.split(', '));
