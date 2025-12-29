0813fa17ba361dee3e66ec3473fd46dd99e7baf3
Aadya Pathak <pathakaadya19@gmail.com>
2025-12-29 22:43:26 +0530

    used for,while,for of loops,forEACH,map,filter,conditionals,ternary operator,switch,functions,arrowfunctions and constructor function

--- main.js
+++ main.js
@@ -2,22 +2,27 @@ console.log("Main.js is loaded and running.");
 alert("Hello from main.js!");
 console.warn("This is a warning");
 alert("This is an alert from main.js");
+
 //let,const, var(not used anymore since its global scope)
 const a=30;
 console.log(a);
+
 //strings,numbers,boolean,null,undefined,symbol
 const name="Aditya";
 const ag= 30;
 const isCool= true;
 const rating=4.5;
-const x = null;
-const y = undefined;
+const c = null;
+const b = undefined;
 let z;
 console.log(typeof z);
+
 //concatenation
-console.log("My name is " + name + " and I am " + age); 
+console.log("My name is " + name + " and I am " + ag); 
+
 //template string
-console.log(`My name is ${name} and I am ${age}`);
+console.log(`My name is ${name} and I am ${ag}`);
+
 //string methods
 const s="Hello World";
 console.log(s.length);
@@ -26,6 +31,7 @@ console.log(s.substring(0,5).toLowerCase());
 console.log(s.split(''));
 const h="technology, computers, it, code";
 console.log(h.split(', '));
+
 //arrays - variables that hold multiple values
 const numbers=new Array(1,2,3,4,5);
 console.log(numbers);
@@ -39,6 +45,7 @@ fruits.pop();//remove last
 console.log(Array.isArray(fruits));
 console.log(fruits.indexOf('oranges'));
 console.log(fruits);
+
 //object literals
 const person= { firstName:'Luigi', 
     lastName:'Mangione',
@@ -68,4 +75,90 @@ console.log(todos);
 console.log(todos[1].text);
 const todoJSON= JSON.stringify(todos);
 console.log(todoJSON);
-//add properties
+
+//for loops
+for(let i=0;i<10;i++){
+    console.log("for loop number: ${i}");}
+
+//while loop
+let i=0;
+while(i,10){
+    console.log("while loop number is: $(i)");
+i++;}
+
+//for..of
+for(let todo of todos){
+    console.log(todo.text);
+}
+
+//forEach, map, filter
+todos.forEach(function(todo){
+    console.log(todo.text);
+});
+const todoText= todos.map(function(todo){
+    return todo.text;
+});
+console.log(todoText);
+const todoCompleted= todos.filter(function(todo){
+    return todo.isCompleted===true;
+});
+console.log(todoCompleted);
+
+//conditionals
+const x='10';
+if(x==10){
+    console.log('x is 10');
+}
+if(x===10){
+    console.log('x is strictly 10');
+}
+const y='20';
+if(x>5 && y>15){
+    console.log('x is more than 5 and y is more than 15');
+}
+
+//ternary operator
+const color= x===10 ? 'red' : 'blue';
+console.log(color);
+
+//switch
+switch(color){
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is not red or blue');
+        break;
+}
+
+//functions
+function addNums(num1=1,num2=1){
+    return num1+num2;
+}
+console.log(addNums(5,5));
+console.log(addNums());
+
+//arrow functions
+const addNums2=(num1=1,num2=1)=> num1+num2;
+console.log(addNums2(10,10));
+console.log(addNums2());
+
+//functions with multiple lines
+function personInfo(person){
+    return `${person.firstName} is ${person.age} years old.`;
+}
+
+//constructor function
+function Person(firstName,lastName,age){
+    this.firstName= firstName;
+    this.lastName= lastName;
+    this.age= age;
+}
+const person1= new Person('Aditya','Waghmare',30);
+const person2= new Person('Luigi','Mangione',25);
+console.log(person1);
+console.log(person2);
+
